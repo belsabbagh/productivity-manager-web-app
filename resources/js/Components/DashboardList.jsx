a78096d6d9dd9fd86bfe3dc9836e82c1e40e8f9f
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-10 18:55:59 +0200

    improved dashboard components

--- resources/js/Components/DashboardList.jsx
+++ resources/js/Components/DashboardList.jsx
@@ -9,26 +9,28 @@ import {
     Typography
 } from "@mui/material";
 import {ListAlt} from "@mui/icons-material";
+import {Link} from "@inertiajs/inertia-react";
 
-export default function DashboardList({title, children}) {
+export default function DashboardList({ className, children, resource}) {
     return (
-        <div className="bg-content p-6 rounded-lg">
+        <div className={className}>
             <div className='flex'>
                 <Typography className='px-auto' variant='h6'>
-                    {title}
+                    {resource}
                 </Typography>
                 <IconButton color="black" component="label">
                     <ListAlt/>
                 </IconButton>
             </div>
 
-            <TableContainer component={Paper}>
-                <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
+            <TableContainer component={Paper} sx={{maxHeight: 150}}>
+                <Table sx={{height: 'max-content' }} size="small" aria-label="a dense table">
                     <TableBody>
                         {children}
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Link href={`/${resource}`}>See all</Link>
         </div>
     );
 }
---
+++ resources/js/Components/DashboardProjectsRow.jsx
@@ -0,0 +1,23 @@
+import {TableCell, TableRow} from "@mui/material";
+import {Link} from "@inertiajs/inertia-react";
+import {ArrowForwardIos} from "@mui/icons-material";
+import React from "react";
+
+export default function DashboardProjectsRow({data}) {
+    const resource = "projects"
+    return (
+        <TableRow
+            className='rounded-lg p-4'
+            style={{backgroundColor: 'rgb(233,231,253)'}}
+            key={data.id}
+        >
+            <TableCell component="th" scope="row">{data.name}</TableCell>
+            <TableCell align="right">{data.utilization}</TableCell>
+            <TableCell align="right">
+                <Link href={`/${resource}/${data.id}`}>
+                    <ArrowForwardIos color="action" className="shadow-md rounded-full"/>
+                </Link>
+            </TableCell>
+        </TableRow>
+    );
+}

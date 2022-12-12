de2c48cf6841fcedc33f242c9c36a9cfa646090d
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-12 11:05:58 +0200

    carousel implemented

--- resources/js/Components/Chart/DoughnutChart.jsx
+++ resources/js/Components/Chart/DoughnutChart.jsx
@@ -1,9 +1,9 @@
 import {Doughnut} from "react-chartjs-2";
 import React from "react";
 
-export default function DoughnutChart({name, data, options}) {
+export default function DoughnutChart({className, name, data, options}) {
     return (
-        <div className={'flex-col justify-center p-3'}>
+        <div className={'flex-col justify-center p-3' + className}>
             <div className={'flex-row justify-center'}>
                 <div className={"w-fit mx-auto"}>{name}</div>
             </div>
--- resources/js/Components/Dashboard/ChartsView.jsx
+++ resources/js/Components/Dashboard/ChartsView.jsx
@@ -1,22 +1,22 @@
-import React from "react";
 import ResourceView from "@/Components/Dashboard/ResourceView";
 import DoughnutChart from "@/Components/Chart/DoughnutChart";
-import 'react-multi-carousel/lib/styles.css';
-import ResourceList from "@/Components/Dashboard/ResourceList";
+import Carousel from 'react-material-ui-carousel';
 
 export default function ChartsView({className, charts}) {
     return (
         <ResourceView className={className} resource={"charts"}>
-            <ResourceList maxHeight={350}>
+            <Carousel height={250} sx={{width: 350, paddingX: 8, paddingTop: 4, paddingBottom: 0}}>
                 <DoughnutChart
+                    className={'h-max'}
                     name={'Project Region Distribution'}
                     data={charts.projectRegionDistribution}
                 />
                 <DoughnutChart
+                    className={'h-max'}
                     name={'Employee Skill Distribution'}
                     data={charts.employeeSkillDistribution}
                 />
-            </ResourceList>
+            </Carousel>
         </ResourceView>
     )
 }

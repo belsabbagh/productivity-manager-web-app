01d5c0db0db95c08feb2e697d8ce00e9a678e9f4
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-16 16:49:56 +0200

    extracted icon

--- resources/js/Pages/teams/edit.jsx
+++ resources/js/Pages/teams/edit.jsx
@@ -3,32 +3,27 @@ import {useForm} from '@inertiajs/inertia-react';
 import {Inertia} from "@inertiajs/inertia";
 import {Button, TextField, Typography} from "@mui/material";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import UserIcon from "@/Components/Icons/UserIcon";
 
 export default function edit(props) {
     const resource = 'team'
     const {assignment} = props
     const {data, setData, errors, put} = useForm({});
-
     function handleSubmit(e) {
         e.preventDefault();
         put(route(`${resource}s.update`, assignment.id));
     }
-
     function destroy() {
         if (confirm(`Are you sure you want to delete this ${resource}?`)) {
             Inertia.delete(route(`${resource}s.destroy`, assignment.id));
         }
     }
-
     function handleChange(e) {
         e.preventDefault();
         const onlyNums = e.target.value
         console.log(onlyNums)
-        if (onlyNums < 0)
-            return
-
+        if (onlyNums < 0) return
         this.setState({value: onlyNums});
-
     }
 
     return (
@@ -45,16 +40,10 @@ export default function edit(props) {
                             <form>
                                 <div id="team member name" className="flex flex-row justify-start mb-2">
                                     <div id="icon css"
-                                         className=" bg-content p-3 border rounded border-gray flex flex-row justify-center items-center">
-
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round"
-                                                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                        </svg>
-
+                                         className=" bg-content p-3 mr-1 border rounded border-gray flex flex-row justify-center items-center">
+                                        <UserIcon svgClassName={'w-6 h-6'}/>
                                     </div>
-                                    <Typography sx={{mb: 2}} variant='h6'> name here </Typography>
+                                    <Typography variant='h6'> name here </Typography>
                                 </div>
                                 <div id="capacity" className="flex flex-row justify-start mb-2">
                                     <div id=" icon css"

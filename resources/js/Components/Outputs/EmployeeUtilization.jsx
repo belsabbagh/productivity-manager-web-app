0cd62f4d419ab77e946c17b310a7a97073134937
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-30 10:13:07 +0200

    small fix

--- resources/js/Components/Outputs/EmployeeUtilization.jsx
+++ resources/js/Components/Outputs/EmployeeUtilization.jsx
@@ -1,13 +1,4 @@
-function formatColor(r, g, b) {
-    return `rgb(${r},${g},${b})`
-}
-
-function getUtilizationColor(val) {
-    const r = (val <= 1) ? 255 * val : 255,
-        g = (val <= 1) ? 255 : 100,
-        b = 100
-    return formatColor(r, g, b)
-}
+import {getUtilizationColor} from "@/lib";
 
 export default function EmployeeUtilization({value, projectCount}){
     return(

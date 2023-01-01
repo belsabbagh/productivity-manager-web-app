8c4112ba78f543491bf7d28491f0b1f30c17c93b
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-01-01 23:48:58 +0200

    removed auto complete

--- resources/js/Pages/team/create.jsx
+++ resources/js/Pages/team/create.jsx
@@ -50,27 +50,6 @@ export default function edit(props) {
                          className=" rounded-lg  bg-content h-[350px]  w-[550px] flex flex-row justify-center pr-8">
 
                         <div id="form" className=" pt-16">
-
-                            <div id="team member" className="flex flex-row justify-start mb-2">
-                                <div id="icon css"
-                                     className=" bg-content p-3 border rounded border-gray flex flex-row justify-center items-center">
-
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                    </svg>
-
-                                </div>
-
-                                <Autocomplete sx={{minWidth: 1}} className="bg-content "
-                                              disablePortal
-                                              id="combo-box-team_leaders"
-                                              options={props.employees}
-                                              getOptionLabel={(i) => i.first_name}
-                                              renderInput={(params) => <TextField {...params}
-                                                                                  label="Team member name"/>}/>
-                            </div>
                             <InputWithIcon
                                 input={<FormControl fullWidth> <InputLabel
                                     id="demo-simple-select-label">Employee</InputLabel>

6922caa6ceacef0ffbe9bf84d6a378edfe708340
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-01-26 18:41:24 +0200

    removed useless files

--- resources/js/Components/IndexContent/EmployeeIndexTableRow.jsx
+++
@@ -1,8 +0,0 @@
-import {formatColor, getUtilizationColor} from "@/lib";
-import {TableCell, TableRow} from "@mui/material";
-import {Link} from "@inertiajs/inertia-react";
-import React from "react";
-
-export default function EmployeeIndexTableRow({employee}) {
-
-}

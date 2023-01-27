16ede4b59ce2a96a3764683ed54f37df375349d2
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-01-27 11:25:14 +0200

    formatted files

--- resources/js/Components/Inputs/CreateResource.jsx
+++ resources/js/Components/Inputs/CreateResource.jsx
@@ -1,12 +1,18 @@
-import {Link} from "@inertiajs/inertia-react";
-import React from "react";
+import {Link} from '@inertiajs/inertia-react';
+import React from 'react';
 
-export default function CreateResource({resource, href=null, label=null}){
-    label = (label===null)?`Create ${resource}`:label
-    href = (href===null)?`/${resource}s/create`:href
-    return (
-        <div>
-            <Link href={href} className="text-white mr-4 px-3 py-2 rounded-lg bg-[#AFACD3] hover:bg-gray font-medium flex items-center justify-between w-full md:w-auto">+ {" "+ label}</Link>
-        </div>
-    )
+export default function CreateResource({resource, href = null, label = null}) {
+  const linkClassName = `
+    text-white bg-[#AFACD3]
+    mr-4 px-3 py-2 
+    rounded-lg hover:bg-gray 
+    font-medium flex items-center justify-between w-full md:w-auto
+    `;
+  return (
+    <div>
+      <Link href={href ?? `/${resource}s/create`} className={linkClassName}>
+        {'+ ' + label ?? `Create ${resource}`}
+      </Link>
+    </div>
+  );
 }
--- resources/js/Components/Inputs/Select/SelectIndex.jsx
+++ resources/js/Components/Inputs/Select/SelectIndex.jsx
@@ -12,10 +12,7 @@ const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
   PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
+    style: {maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP, width: 250},
   },
 };
 
